b0481c3224ef42d3e411a04e0338d44fa28ebfad
Mihir Shrestha <121042506+MihiRrr24@users.noreply.github.com>
2025-07-15 09:59:54 +0530

    Updated

---
+++ data/meals.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 'v1',
+    title: 'Paneer Butter Masala',
+    imageUrl: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/02/paneer-butter-masala-1.jpg',
+    duration: 35,
+    complexity: 'Medium',
+  },
+  {
+    id: 'v2',
+    title: 'Vegetable Biryani',
+    imageUrl: 'https://www.cookwithmanali.com/wp-content/uploads/2020/07/Vegetable-Biryani-Instant-Pot.jpg',
+    duration: 45,
+    complexity: 'Hard',
+  },
+  {
+    id: 'v3',
+    title: 'Aloo Gobi',
+    imageUrl: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2022/08/aloo-gobi.jpg',
+    duration: 25,
+    complexity: 'Simple',
+  },
+  {
+    id: 'v4',
+    title: 'Masala Dosa',
+    imageUrl: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/08/masala-dosa-recipe-2.jpg',
+    duration: 30,
+    complexity: 'Medium',
+  },
+  {
+    id: 'v5',
+    title: 'Palak Paneer',
+    imageUrl: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/palak-paneer-recipe.jpg',
+    duration: 35,
+    complexity: 'Medium',
+  },
+  {
+    id: 'v6',
+    title: 'Veg Manchurian',
+    imageUrl: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2022/01/veg-manchurian-recipe.jpg',
+    duration: 40,
+    complexity: 'Medium',
+  },
+];
